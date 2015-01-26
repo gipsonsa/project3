fdce81fb70b21ffd7c829b22c99d1f00d65510d2
Samuel Gipson <samuelgipson@Samuels-MacBook-Pro.local>
2015-01-26 17:45:34 -0500

    routed '/' to views/index.html

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@
 require 'sinatra'
 
 class MyWebApp < Sinatra::Base
-  get '/' do
-    "Welcome to the webapp of Spider Nation, where the copyright is always up to date! Copyright 2014-#{ Time.now.year } Samuel Gipson."
-  end
+	get '/' do
+  		send_file 'views/index.html'
+	end
 end
